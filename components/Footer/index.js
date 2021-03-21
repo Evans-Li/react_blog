7d7674d994365c3319aa8031eeef94f35e9f0c89
Axing <lx1587787449@gmail.com>
2021-03-21 15:41:52 +0800

    修改footer

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 const Footer = ()=>(
     <div className="footer-div">
-        <div>系统由 React+Node+Ant Desgin驱动 </div>
+        <div> 系统由 React+Node+Ant Desgin驱动 </div>
         <div>-- 最好的时机就是当下 --</div>
     </div>
 )
